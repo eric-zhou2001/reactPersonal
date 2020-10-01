a1005fce63fc6443fdd04cc8e52d5f204f4ee46c
Eric Zhou <59323669+eric-zhou2001@users.noreply.github.com>
2020-09-30 22:01:09 -0400

    began building react

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends React.Component {
+    render() {
+        return(
+            <p>
+                <span color="red">Eric</span>
+            </p>
+        )
+    }
+}
+
+export default Home;
